93e4c7da1b04e6ab3d956b2b746edc942ffef771
clinton <clinton.page123@gmail.com>
2025-02-26 19:59:38 +1030

    put instructions in hello world on how to run the code

--- 1-hello-world/1-hello-world.go
+++ 1-hello-world/1-hello-world.go
@@ -7,6 +7,8 @@ import (
 )
 
 func main() {
+	// can run with "go run ."
+	// this will compile all the files in the current folder and run the binary
 	const age = 29
 	var name string = "abc"
 	const unused float64 = age
